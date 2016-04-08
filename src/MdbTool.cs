f05ec79bccf8c5c34d3441c462af83f3cdb67da9
Esun Kim <veblush@gmail.com>
2016-04-08 22:56:27 +0900

    Make ilpack-tolerable

--- src/MdbTool.cs
+++ src/MdbTool.cs
@@ -4,13 +4,7 @@
     {
         public static void ConvertPdbToMdb(string dll)
         {
-            RunPdb2Mdb(dll);
-        }
-
-        private static void RunPdb2Mdb(params string[] args)
-        {
-            var entryPoint = typeof(Pdb2Mdb.Converter).Assembly.EntryPoint;
-            entryPoint.Invoke(null, new object[] { args });
+            Pdb2Mdb.Converter.Convert(dll);
         }
     }
 }
