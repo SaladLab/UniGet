830834cad620522ecd2fac84e8a01f4a55e74ed7
Esun Kim <veblush@gmail.com>
2016-04-25 15:08:19 +0900

    Prevent missing pdb exception

--- src/UniGet/MdbTool.cs
+++ src/UniGet/MdbTool.cs
@@ -1,10 +1,14 @@
-﻿namespace UniGet
+﻿using System.IO;
+
+namespace UniGet
 {
     internal class MdbTool
     {
         public static void ConvertPdbToMdb(string dll)
         {
-            Pdb2Mdb.Converter.Convert(dll);
+            var pdbFile = Path.ChangeExtension(dll, ".pdb");
+            if (File.Exists(pdbFile))
+                Pdb2Mdb.Converter.Convert(dll);
         }
     }
 }
